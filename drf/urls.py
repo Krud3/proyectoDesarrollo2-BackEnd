af64fcab718316bad31b9db6409751e60d5bfb3f
ErvinCaraval <ervin.caravali@correounivalle.edu.co>
2024-04-29 18:41:52 -0500

    se actuliza la api

--- drf/urls.py
+++ drf/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    
     path('auction_app/v1/', include('auction_app.urls')),
 ]

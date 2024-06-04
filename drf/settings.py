78ceab381322a8f9dc9707aab4473c317bae4d4d
ErvinCaraval <ervin.caravali@correounivalle.edu.co>
2024-06-04 16:05:43 -0500

    se mejora seguridad de codigo

--- drf/settings.py
+++ drf/settings.py
@@ -12,15 +12,16 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subd'
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e4h5$(htupo01qm!x3ml0sf0mpg)$=n=)en(-7cqm%*u_2fl)w'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

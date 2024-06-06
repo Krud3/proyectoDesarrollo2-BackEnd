813a9272efee514c4231ce77c0d87fd8b6b7d352
Juan Molina <juan.sebastian.molina@correounivalle.edu.co>
2024-06-05 21:10:54 -0500

    added config to settings.py to read the database from heroku

--- drf/settings.py
+++ drf/settings.py
@@ -12,16 +12,16 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subd'
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv()
+#load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
